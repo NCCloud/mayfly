cabcc34ce824cceae6e6f4321aa2522edf15476e
Yunus Sandikci <yunussandikci@outlook.com>
2024-01-20 14:25:22 +0300

    .

--- pkg/common/config.go
+++ pkg/common/config.go
@@ -1,13 +1,14 @@
 package common
 
 import (
+	"github.com/caarlos0/env/v10"
 	"time"
 )
 
 type Config struct {
 	EnableLeaderElection bool          `env:"ENABLE_LEADER_ELECTION" envDefault:"false"`
-	SyncPeriod           time.Duration `env:"SYNC_PERIOD" envDefault:"30m"`
-	MonitoringInterval   time.Duration `env:"MONITORING_INTERVAL" envDefault:"1s"`
+	SyncPeriod           time.Duration `env:"SYNC_PERIOD" envDefault:"60m"`
+	MonitoringInterval   time.Duration `env:"MONITORING_INTERVAL" envDefault:"5s"`
 	ExpirationLabel      string        `env:"EXPIRATION_LABEL" envDefault:"mayfly.cloud.namecheap.com/expire"`
 	Resources            []string      `env:"RESOURCES" envSeparator:"," envDefault:"v1;Secret,cloud.namecheap.com/v1alpha1;ScheduledResource"`
 }
